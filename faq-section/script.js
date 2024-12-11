0182833dfb21f112c428d47f7f1851230c4e8e59
marphic-digital <marphicdigital@gmail.com>
2024-12-11 20:56:22 +0500

    js-added

---
+++ faq-section/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const questions = document.querySelectorAll(".question");
+const faqBox = document.querySelectorAll(".faq-box");
+
+questions.forEach((question, i) => {
+  question.addEventListener("click", () => {
+    faqBox.forEach((box) => {
+      box.classList.remove("show-ans");
+    });
+    faqBox[i].classList.toggle("show-ans");
+  });
+});
