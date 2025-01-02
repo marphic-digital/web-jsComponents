a70c6ce172d46ecfc7a5dfbfb3efeb3eab2c4c76
marphic-digital <marphicdigital@gmail.com>
2025-01-02 13:11:49 +0500

    functionality

--- book-a-seat/script.js
+++ book-a-seat/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.querySelector('#movie');
+let ticketPrice = +movieSelect.value;
+
+function updateSelectedCount(){
+    const selectedSeatsCount = document.querySelectorAll('.row .seat.selected')
+    const selectedSeatsLength = selectedSeatsCount.length;
+    
+    count.innerText = selectedSeatsLength;
+    total.innerText = selectedSeatsLength * ticketPrice;
+}
+
+movieSelect.addEventListener('change', (e) => {
+    ticketPrice = e.target.value;
+    updateSelectedCount();
+})
+
+container.addEventListener('click', function(e){
+    e.preventDefault();
+    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+        e.target.classList.toggle('selected')
+    }
+    updateSelectedCount();
+})
