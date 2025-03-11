32677f6063758c742b066ef11462547d02ae73eb
marphic-digital <156898139+marphic-digital@users.noreply.github.com>
2025-03-11 15:30:36 +0000

    drop-trail-animation

---
+++ heart-click-event/script.js
@@ -0,0 +1,16 @@
+const bodyEl = document.querySelector('body');
+
+bodyEl.addEventListener('mousemove', (event) => {
+    const xPos = event.offsetX;
+    const yPos = event.offsetY;
+    const spanEl = document.createElement('span');
+    spanEl.style.left = xPos + 'px';
+    spanEl.style.top = yPos + 'px';
+    const dropSize = Math.random() * 100;
+    spanEl.style.width = dropSize + "px";
+    spanEl.style.height = dropSize + 'px';
+    bodyEl.appendChild(spanEl);
+    setTimeout(() => {
+        spanEl.remove();
+}, 2000)
+})
