89d234912cffd9c6fef1d9133e68885c99aee24b
marphic-digital <156898139+marphic-digital@users.noreply.github.com>
2025-04-17 09:28:40 +0000

    swiper-slide

---
+++ swiper-slider-1/script.js
@@ -0,0 +1,24 @@
+let swiper = new Swiper ('.swiper', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    initialSlides: 2,
+    speed: 600,
+    preventClicks: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 80,
+        depth: 350,
+        modifier: 1,
+        slideShadow: true,
+    },
+    on: {
+        click(event) {
+            swiper.slideTo(this.clickedIndex)
+        }
+    },
+    pagination: {
+        el: '.swiper-pagination'
+    },
+})
